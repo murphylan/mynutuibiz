73c599585746391c46c3a505e923cfe63ccce4ef
lanzejun <lanzejun@cn.ibm.com>
2023-09-16 20:04:03 +0800

    update

--- src/custom-tab-bar/index.tsx
+++ src/custom-tab-bar/index.tsx
@@ -1,46 +1,46 @@
 import { Cart, Category, Find, Home, My } from '@nutui/icons-react-taro';
 import { Tabbar } from '@nutui/nutui-react-taro';
 import { useContext, useState } from 'react';
-
+import Taro from '@tarojs/taro';
 import UserContext from '../UserContext';
 import './index.scss';
-import Taro from '@tarojs/taro';
 
 export default function index() {
 
   const { selectedTab, setSelectedTab } = useContext(UserContext);
 
+  const iconSize = { width: 12, height: 12 };
   const [tabData, setTabData] = useState({
     list: [
       {
         id: 0,
-        icon: <Home width={12} height={12} />,
+        icon: <Home {...iconSize} />,
         text: '首页',
-        url: 'pages/index/index',
+        url: '/pages/index/index',
       },
       {
         id: 1,
-        icon: <Category width={12} height={12} />,
+        icon: <Category {...iconSize} />,
         text: '分类',
-        url: 'pages/cate/index',
+        url: '/pages/cate/index',
       },
       {
         id: 2,
-        icon: <Find width={12} height={12} />,
+        icon: <Find {...iconSize} />,
         text: '发现',
-        url: 'pages/hot/index',
+        url: '/pages/hot/index',
       },
       {
         id: 3,
-        icon: <Cart width={12} height={12} />,
+        icon: <Cart {...iconSize} />,
         text: '购物车',
-        url: 'pages/cart/index',
+        url: '/pages/cart/index',
       },
       {
         id: 4,
-        icon: <My width={12} height={12} />,
+        icon: <My {...iconSize} />,
         text: '我的',
-        url: 'pages/my/index',
+        url: '/pages/my/index',
       },
     ]
   });
@@ -48,19 +48,17 @@ export default function index() {
   const onSwitch = (e) => {
     setSelectedTab(e)
     Taro.switchTab({
-      url: '/' + tabData.list[e].url
+      url: tabData.list[e].url
     })
   }
 
   return (
-    <>
-      <Tabbar value={selectedTab} onSwitch={onSwitch} >
-        {tabData.list.map(item => {
-          return (
-            <Tabbar.Item title={item.text} key={item.id} icon={item.icon} />
-          )
-        })}
-      </Tabbar>
-    </>
+    <Tabbar value={selectedTab} onSwitch={onSwitch} >
+      {tabData.list.map(item => {
+        return (
+          <Tabbar.Item title={item.text} key={item.id} icon={item.icon} />
+        )
+      })}
+    </Tabbar>
   )
 }
